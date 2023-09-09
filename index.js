d88437b315cb6c25f019f07fe9f7db64124695cc
raji <kingraj1344@gmail.com>
2023-09-09 14:20:54 +0100

    done

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const port = process.env.PORT || 3000;
 // Define a route
 app.get('/', (req, res) => {
     const body = req.query
-    res.status(200).json(body);
+    const date = new Date()
+    const utc_time = date.toUTCString()
+    const github_file_url = "https://github.com/raji2004/zuri-backend/index.js"
+    const github_repo_url = "https://github.com/raji2004/zuri-backend"
+    const status_code = 200
+    res.status(200).json({ current_day: "Saturday", utc_time,github_file_url,github_repo_url,status_code, ...body });
 });
 
 // Start the server
