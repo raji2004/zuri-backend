15dfb80e25dffe8b3e5b74adef2641343ad824c8
raji <kingraj1344@gmail.com>
2023-09-09 14:26:07 +0100

    d

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     const body = req.query
     const date = new Date()
     const utc_time = date.toUTCString()
-    const github_file_url = "https://github.com/raji2004/zuri-backend/index.js"
+    const github_file_url = "https://github.com/raji2004/zuri-backend/blob/main/index.js"
     const github_repo_url = "https://github.com/raji2004/zuri-backend"
     const status_code = 200
     res.status(200).json({ ...body,current_day: "Saturday", utc_time,github_file_url,github_repo_url,status_code,  });
