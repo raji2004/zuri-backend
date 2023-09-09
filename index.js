d3ef0d0ba795535c49613a6f4ca540465ecbdfce
raji <kingraj1344@gmail.com>
2023-09-09 14:47:41 +0100

    d

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+
+
 // Define a route
 app.get('/',(req,res)=>{
     res.send('omo')
